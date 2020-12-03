e63bf26398821e4b917aeda9a2c503e3e8df7f0c
Kalpak <kals9seals@gmail.com>
2020-12-02 19:43:51 -0500

    Test CNN written

--- convNet.py
+++ convNet.py
@@ -61,3 +61,8 @@ def accuracy(outputs, labels):
     """
     outputs = np.argmax(outputs, axis=1)
     return np.sum(outputs == labels) / float(labels.size)
+
+
+metrics = {
+    'accuracy': accuracy,
+}
