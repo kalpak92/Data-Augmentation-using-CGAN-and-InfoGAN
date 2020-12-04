460fd01f840999a5ddd5033a87aa2443d89376ea
Shantanu Ghosh <beingshantanu2406@gmail.com>
2020-12-04 16:18:11 -0500

    plots

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -5,12 +5,12 @@ class Constants:
     MNIST_disc_c_dim = 10
     MNIST_num_con_c = 2
 
-    INFO_GAN_TRAIN_IMAGE_PATH = "./Info_GAN_Images/Training_Images_before_Training_{}"
-    INFO_GAN_TRAIN_IMAGE_PATH_AFTER_TRAINING = "./Info_GAN_Images/Training_Images_after_Training_%d_{}"
-    INFO_GAN_ANIM_PATH = "./Info_GAN_Images/infoGAN_{}.gif"
+    INFO_GAN_TRAIN_IMAGE_PATH = "Info_GAN_Plots/Training_Images_before_Training_{}"
+    INFO_GAN_TRAIN_IMAGE_PATH_AFTER_TRAINING = "Info_GAN_Plots/Training_Images_after_Training_%d_{}"
+    INFO_GAN_ANIM_PATH = "Info_GAN_Plots/infoGAN_{}.gif"
 
-    INFO_GAN_LOSS_PATH = "./Info_GAN_Images/Loss Curve {}"
-    INFO_GAN_TRAIN_IMAGE_PER_EPOCH_PATH = "./Info_GAN_Images/Generated_image_per_epoch"
+    INFO_GAN_LOSS_PATH = "Info_GAN_Plots/Loss Curve {}"
+    INFO_GAN_TRAIN_IMAGE_PER_EPOCH_PATH = "Info_GAN_Plots/Generated_image_per_epoch"
     INFO_GAN_LR = 2e-4
     INFO_GAN_EPOCH = 100
 
